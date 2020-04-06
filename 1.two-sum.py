e0f25fce20ac747a857f438f21fdc558f2f08e7b
zzc c <zhiye1@hotmail.com>
2020-04-06 21:36:31 +0800

    no1

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -33,6 +33,11 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for idx in range(len(nums)):
+            if (target - nums[idx]) in dic:
+                return [dic[target - nums[idx]], idx]
+            dic[nums[idx]] = idx
         
 # @lc code=end
 
