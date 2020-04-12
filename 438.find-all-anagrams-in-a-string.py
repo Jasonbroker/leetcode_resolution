58e752663947bef7866d868e7453e26466ed6903
zzc c <zhiye1@hotmail.com>
2020-04-13 01:04:27 +0800

    438

--- 438.find-all-anagrams-in-a-string.py
+++ 438.find-all-anagrams-in-a-string.py
@@ -52,8 +52,55 @@
 #
 
 # @lc code=start
+
+
+
+
+
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        # first deal with the p count as map {'ascii': count, ....}
+        ori = [0]*26 # 生成26个字母长度的数组
         
+        a = ord('a')
+        for val in p:
+            ori[ord(val) - a] += 1
+        
+        finder = [0]*26 # 生成26个字母长度的数组
+        
+        l, r = 0, 0
+        result = []  # record the result
+        while r < len(s):
+            rv = ord(s[r]) - a
+            if ori[rv] == 0: ## 原串没有这个值，只能直接跳过了
+                r += 1
+                l = r
+                for x in range(26) : finder[x] = 0
+                continue
+            if r - l < len(p) - 1:
+                if finder[rv] and finder[rv] >= ori[rv]: # 当前这个还没加呢，但是加了就超过，只能l增加
+                    lv = ord(s[l]) - a
+                    finder[lv] -= 1
+                    l += 1
+                else :
+                    finder[rv] += 1
+                    r += 1
+            elif r - l == len(p) - 1:
+                lv = ord(s[l]) - a
+                if finder[rv] + 1 == ori[rv]:
+                    result.append(l)
+                    finder[lv] -= 1
+                    finder[rv] += 1
+                    l += 1
+                    r += 1
+                else:
+                    finder[lv] -= 1
+                    l += 1
+
+        return result
+        
+
+
 # @lc code=end
 
